6cee7d4ac5946147768801f08993428116f9f915
Aman Ruhela <89697559+amanruhela1@users.noreply.github.com>
2023-10-31 12:36:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#text', {
+    strings: ['Student', 'Flutter Developer', 'Software Engineer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay:1000,
+    fadeOut: 1000,
+    loop: true
+});
